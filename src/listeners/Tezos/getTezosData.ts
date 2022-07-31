71864bd0cd6177f8a47f6094267d6d7ab35783bc
ronyXnes <derrarony@gmail.com>
2022-07-31 19:07:56 +0300

    tezos col data

--- src/listeners/Tezos/getTezosData.ts
+++ src/listeners/Tezos/getTezosData.ts
@@ -7,12 +7,19 @@ import axios from "axios";
 
 export const getTezosCollectionData = async (hash: string) => {
     try {
-        const data = await axios.get(`https://api.tzkt.io/v1/operations/${hash}`)
+        let data
+        for(let i= 0 ; i < 20 ; i++){
+            data = await axios.get(`https://api.tzkt.io/v1/operations/${hash}`)
+            if(data){
+                console.log(i , "times to get tezos collection data")
+                break;
+            }
+        }
         if (!data) return;
 
         const entrypoint = data.data[0].parameter?.entrypoint;
         const tokenId = data.data[0].parameter?.value?.token_id
-        
+
         let contractAdd = "";
         let collectionName = "";
 
