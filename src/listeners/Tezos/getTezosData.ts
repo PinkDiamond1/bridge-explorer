e8672f6b7e931c90e2b8b6c6d4a08de431fa05f6
ronyXnes <derrarony@gmail.com>
2022-08-01 11:57:39 +0300

    get tezos data

--- src/listeners/Tezos/getTezosData.ts
+++ src/listeners/Tezos/getTezosData.ts
@@ -1,59 +1,59 @@
 import axios from "axios";
-// import createEventRepo from "../../business-logic/repo";
-// import { BlockRepo } from "../../Intrerfaces/IBlockRepo";
-// import { IDatabaseDriver, Connection, wrap, EntityManager } from "@mikro-orm/core";
-// import { BigMapAbstraction, MichelsonMap, TezosToolkit } from "@taquito/taquito";
-// import { bytes2Char } from "@taquito/utils";
 
 export const getTezosCollectionData = async (hash: string) => {
     try {
-        let data
-        for(let i= 0 ; i < 20 ; i++){
+        let contractAdd;
+        let collectionName;
+        let contractData;
+        let data;
+        let uri;
+
+        for (let i = 0; i < 20; i++) {
             data = await axios.get(`https://api.tzkt.io/v1/operations/${hash}`)
-            if(data?.data){
-                console.log(i , "times to get tezos collection data")
+            if (data?.data) {
                 break;
             }
         }
         if (!data) return;
 
-        const entrypoint = data.data[0].parameter?.entrypoint;
-        const tokenId = data.data[0].parameter?.value?.token_id
-
-        let contractAdd = "";
-        let collectionName = "";
+        const entrypoint = data.data[0]?.parameter?.entrypoint;
+        const tokenId = data.data[0]?.parameter?.value?.token_id
 
         if (entrypoint === "withdraw_nft") {
-            contractAdd = data.data[0].parameter?.value?.burner;
+            contractAdd = data.data[0]?.parameter?.value?.burner;
             collectionName = "WNFT";
         } else {
             contractAdd = data.data[1]?.target?.address;
-            collectionName = data.data[1]?.target?.alias.toUpperCase();
+            collectionName = data.data[1]?.target?.alias?.toUpperCase();
+        }
+
+        if (!collectionName && contractAdd) {
+            for (let i = 0; i < 20; i++) {
+                contractData = await axios.get(`https://api.better-call.dev/v1/contract/mainnet/${contractAdd}`)
+                if (contractData?.data) {
+                    collectionName = contractData?.data?.alias?.toUpperCase();
+                    break;
+                }
+            }
+        }
+
+        if (tokenId && contractAdd) {
+            for (let i = 0; i < 20; i++) {
+                let uriData = await axios.get(`https://api.better-call.dev/v1/tokens/mainnet/metadata?token_id=${tokenId}&contract=${contractAdd}`)
+                if (uriData?.data) {
+                    uri = uriData?.data[0]?.display_uri ||  uriData?.data[0]?.artifact_uri || uriData?.data[0]?.thumbnail_uri
+                    break;
+                }
+            }
         }
 
         console.log("tokenId:", tokenId);
         console.log("contractAdd:", contractAdd);
         console.log("collectionName:", collectionName);
+        console.log("uri" ,uri )
 
-        return { tokenId, contractAdd, collectionName }
+        return { tokenId, contractAdd, collectionName, uri }
     } catch (err) {
         console.log(err)
     }
 }
-// export async function getUriFa2(fa2Address: string, tokenId: string , rpc: string): Promise<string> {
-//   const tezos = new TezosToolkit(rpc);
-
-//     const contract = await tezos.contract.at(fa2Address);
-//     const storage = await contract.storage<{
-//       token_metadata: BigMapAbstraction;
-//     }>();
-
-//     const tokenStorage = await storage.token_metadata.get<{
-//       token_info: MichelsonMap<string, string>;
-//     }>(tokenId);
-
-//     const uriFa2 = bytes2Char(tokenStorage!.token_info.get("")!);
-//     console.log("tezos.ts Line 72 - Uri:", uriFa2)
-
-//     return uriFa2;
-//   }
